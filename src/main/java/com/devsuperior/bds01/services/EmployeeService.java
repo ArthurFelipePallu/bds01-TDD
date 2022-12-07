747ec9059c3e2c4cd2b31105777eb0a93a88b166
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-12-07 14:29:46 -0300

    tried up to this point

--- src/main/java/com/devsuperior/bds01/services/EmployeeService.java
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -1,6 +1,9 @@
+package com.devsuperior.bds01.services;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
@@ -9,6 +12,7 @@ import com.devsuperior.bds01.entities.Employee;
 import com.devsuperior.bds01.repositories.DepartmentRepository;
 import com.devsuperior.bds01.repositories.EmployeeRepository;
 
+@Service
 public class EmployeeService {
 	
 	@Autowired
